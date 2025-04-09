13e7a84f2baeb262086fc7aaa7766bef2a1c5c50
taewoocode <taewoocode@gmail.com>
2025-04-09 21:53:57 +0900

    BM-6 [feat]: JWT 토큰 필터 생성

---
+++ src/main/java/com/bidmall/gateway/filter/JwtAuthenticationFilter.java
@@ -0,0 +1,60 @@
+package com.bidmall.gateway.filter;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import reactor.core.publisher.Mono;
+
+@Component
+public class JwtAuthenticationFilter implements GlobalFilter {
+	@Value("${jwt.secret}")
+	private String secret;
+
+	@Override
+	public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+		ServerHttpRequest request = exchange.getRequest();
+		String authHeader = request.getHeaders().getFirst(HttpHeaders.AUTHORIZATION);
+		if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+			return unauthorized(exchange);
+		}
+
+		String token = authHeader.substring(7);
+		try {
+			return chain.filter(exchange.mutate()
+				.request(getHeader(exchange, parsingToken(token))).build());
+		} catch (JwtException e) {
+			return unauthorized(exchange);
+		}
+	}
+
+	private Claims parsingToken(String token) {
+		Claims claims = Jwts.parser()
+			.setSigningKey(secret)
+			.parseClaimsJws(token)
+			.getBody();
+		return claims;
+	}
+
+	private static ServerHttpRequest getHeader(ServerWebExchange exchange, Claims claims) {
+		ServerHttpRequest request;
+		request = exchange.getRequest().mutate()
+			.header("X-User-Id", claims.getSubject())
+			.build();
+		return request;
+	}
+
+	private Mono<Void> unauthorized(ServerWebExchange exchange) {
+		exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+		return exchange.getResponse().setComplete();
+	}
+}
+
