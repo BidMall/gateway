758fa6c0331ec300103063b10dc14f43d6cd8e91
taewoocode <taewoocode@gmail.com>
2025-04-13 00:03:23 +0900

    BM-6 [feat]: 토큰생성 예외처리 추가

--- src/main/java/com/bidmall/gateway/filter/TokenProvider.java
+++ src/main/java/com/bidmall/gateway/filter/TokenProvider.java
@@ -3,15 +3,19 @@ package com.bidmall.gateway.filter;
 import java.util.Date;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
 
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
-@Service
+@Component
 @Slf4j
+@RequiredArgsConstructor
 public class TokenProvider {
+
+	private final JwtTokenValidator jwtTokenValidator;
 
 	@Value("${jwt.secret}")
 	private String secret;
@@ -22,14 +26,21 @@ public class TokenProvider {
 	/**
 	 * 토큰생성
 	 * @param userId
-	 * @return
+	 * @return 생성된 토큰
 	 */
 	public String createToken(String userId) {
-		return Jwts.builder()
+		String createToken = Jwts.builder()
 			.setSubject(userId)
 			.setExpiration(new Date(System.currentTimeMillis() + expiration))
 			.signWith(SignatureAlgorithm.HS512, secret)
 			.compact();
-	}
 
+		try {
+			jwtTokenValidator.validationToken(createToken);
+		} catch (Exception e) {
+			log.error("토큰 검증 실패: {}", e.getMessage());
+			throw new RuntimeException("토큰 검증 실패", e);
+		}
+		return createToken;
+	}
 }
