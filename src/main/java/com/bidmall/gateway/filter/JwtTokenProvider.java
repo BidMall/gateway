8652be4a44d206d3fd694b42732a2a6239e08691
taewoocode <taewoocode@gmail.com>
2025-04-13 02:17:21 +0900

    BM-6 [feat]: 토큰생성로직 추가

--- src/main/java/com/bidmall/gateway/filter/JwtTokenProvider.java
+++ src/main/java/com/bidmall/gateway/filter/JwtTokenProvider.java
@@ -1,10 +1,13 @@
 package com.bidmall.gateway.filter;
 
+import java.util.Date;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 
 @Component
 public class JwtTokenProvider {
@@ -12,11 +15,28 @@ public class JwtTokenProvider {
 	@Value("${jwt.secret}")
 	private String secret;
 
+	@Value("${jwt.expiration}")
+	private long expiration; // ms
+
 	public Claims parsingToken(String token) {
 		Claims claims = Jwts.parser()
 			.setSigningKey(secret)
 			.parseClaimsJws(token)
 			.getBody();
 		return claims;
+	}
+
+	/**
+	 * 토큰생성
+	 */
+	public String generateToken(String userId) {
+		Date now = new Date();
+		Date expiryDate = new Date(now.getTime() + expiration);
+		return Jwts.builder()
+			.setSubject(userId)
+			.setIssuedAt(now)
+			.setExpiration(expiryDate)
+			.signWith(SignatureAlgorithm.HS256, secret)
+			.compact();
 	}
 }
